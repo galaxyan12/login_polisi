176bf39d5cab8611f4af40d06a047d72105a7cb8
galaxyan12 <jody@student.untan.ac.id>
2021-04-19 10:50:23 +0700

    update icon

--- lib/page/login_page.dart
+++ lib/page/login_page.dart
@@ -95,14 +95,14 @@ class _LoginPageState extends State<LoginPage> {
 
   @override
   Widget build(BuildContext context) {
-    final logo = Hero(
-      tag: 'hero',
-      child: CircleAvatar(
-        backgroundColor: Colors.transparent,
-        radius: 48.0,
-        child: Image.asset('assets/images/logoapp.png'),
-      ),
-    );
+    // final logo = Hero(
+    //   tag: 'hero',
+    //   child: CircleAvatar(
+    //     backgroundColor: Colors.transparent,
+    //     radius: 48.0,
+    //     child: Image.asset('assets/images/logoapp.png'),
+    //   ),
+    // );
 
     final nrpField = TextFormField(
       validator: (value) {
